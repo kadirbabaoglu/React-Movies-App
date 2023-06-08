327873b57271b8f9d6109fd5628612387e192cdb
Kadir Babaoglu <kadirbabaoglu@outlook.com>
2023-06-08 08:42:02 +0300

    add search method

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -4,7 +4,6 @@ import Badge from 'react-bootstrap/Badge';
 
 
 const MovieList = (props) => {
-   console.log(props)
    
     return (
 
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -4,20 +4,27 @@ import React from "react";
 //---------- import Bootstrap --------
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const SearchBar = () => {
 
 
+const SearchBar = (props) => {
+  
+  const CloseOnSubmit = (e) =>{
+    e.preventDefault();
+  }
   return (
-    
+
+    <form onSubmit={CloseOnSubmit}>
       <div className="row">
-        <div  className="mb-3 text-center">
-        <h1>Movie Search APP</h1>
-        <input type="text" className="form-control"  placeholder="Search Movie" />
+        <div className="mb-3 text-center">
+          <h1>Movie Search APP</h1>
+          <input onChange={props.searchMovies} type="text" className="form-control" placeholder="Search Movie" />
+        </div>
       </div>
-      </div>
-    
+    </form>
+
   );
 }
 
 
 export default SearchBar;
+
